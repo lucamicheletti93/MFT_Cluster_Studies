db26c4bec64babe7f7ccda2c9d818b3b35b69004
Lucamicheletti93 <luca.mike93@gmail.com>
2022-10-26 22:06:49 +0200

    Adding the code to run on merged files

--- run_MFTAssesment.py
+++ run_MFTAssesment.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import yaml
 import random
+import ROOT
 
 def run_mft_assesment(inputCfg, mode):
     '''
@@ -25,34 +26,54 @@ def run_mft_assesment(inputCfg, mode):
         counter = counter + 1
         print("----------------------> %s processed" % (path))
 
-    #if not os.path.isdir(inputCfg["output"]["output_dir"]) :
-        #print("the directory does not exist, creating %s" % (inputCfg["output"]["output_dir"]))
-        #os.system("mkdir -p %s" % (inputCfg["output"]["output_dir"]))
+def merge_files(inputCfg):
+    '''
+    function for merging files
+    '''
+    paths = []
+    counter = 0
+    for file in os.listdir(inputCfg["input"]["input_path"]):
+        d = os.path.join(inputCfg["input"]["input_path"], file)
+        if os.path.isdir(d) and os.path.isfile("%s/MFTAssessment.root" % (d)):
+            if counter == 0:
+                mergeCommand = "hadd -f MFTAssessment.root "
+            mergeCommand += "%s/MFTAssessment.root " % (d)
+            counter = counter + 1
+    
+    print(mergeCommand)
+    os.system(mergeCommand)
 
-    #if mode == "test" :
-        #fIn  = open(inputCfg["input"]["run_list_file"], "r")
+def plots():
+    '''
+    function for producing QC plots
+    '''
+    fIn = ROOT.TFile.Open("MFTAssessment.root", "READ")
+    mMFTTrueTrackMeanClusterSize = fIn.Get("mMFTTrueTrackMeanClusterSize")
+    mMFTTrueTrackMeanClusterSize.SetLineColor(ROOT.kBlack)
+    mMFTTrueTrackMeanClusterSize.Rebin(10)
+    mMFTTrueTrackMeanClusterSizeNuclei = fIn.Get("mMFTTrueTrackMeanClusterSizeNuclei")
+    mMFTTrueTrackMeanClusterSizeNuclei.SetLineColor(ROOT.kRed)
+    mMFTTrueTrackMeanClusterSizeNuclei.Rebin(10)
+    mMFTTrueTrackMeanClusterSizeNonNuclei = fIn.Get("mMFTTrueTrackMeanClusterSizeNonNuclei")
+    mMFTTrueTrackMeanClusterSizeNonNuclei.SetLineColor(ROOT.kBlue)
+    mMFTTrueTrackMeanClusterSizeNonNuclei.Rebin(10)
 
-    #if mode == "full" :
-        #fIn  = open(inputCfg["input"]["run_list_file"], "r")
-        #fOut = open("%s/%s" % (inputCfg["output"]["output_dir"], inputCfg["output"]["submitted_output_file"]), "w")
+    canvas = ROOT.TCanvas("canvas", "canvas", 600, 600)
+    mMFTTrueTrackMeanClusterSize.Draw("H")
+    mMFTTrueTrackMeanClusterSizeNuclei.Draw("Hsame")
+    mMFTTrueTrackMeanClusterSizeNonNuclei.Draw("Hsame")
+    canvas.Update()
 
-    #if mode == "terminate" :
-        #if not os.path.isfile("%s/%s" % (inputCfg["output"]["output_dir"], inputCfg["output"]["submitted_output_file"])) :
-            #print('Submitted jobs file does not exist! Do --full before')
-            #return
-        #fIn  = open("%s/%s" % (inputCfg["output"]["output_dir"], inputCfg["output"]["submitted_output_file"]), "r")
-        #fOut = open("%s/%s" % (inputCfg["output"]["output_dir"], inputCfg["output"]["terminated_output_file"]), "w")
-
-    #for run in fIn:
-        #print(fr"aliroot -b -q %s\(\"%s\",%i\)" % (inputCfg["input"]["macro_to_run"], mode, int(run)))
-        #if mode == "test" :
-            #break
-        #fOut.write(run)
+    fOut = ROOT.TFile.Open("MyAnalysisResults.root", "RECREATE")
+    canvas.Write()
+    fOut.Close()
 
 def main():
     parser = argparse.ArgumentParser(description='Arguments to pass')
     parser.add_argument('cfgFileName', metavar='text', default='config.yml', help='config file name')
-    parser.add_argument("--run", help="test your task", action="store_true")
+    parser.add_argument("--run_full", help="run over all the available files", action="store_true")
+    parser.add_argument("--merge", help="merge all the files produced", action="store_true")
+    parser.add_argument("--plot", help="plot results", action="store_true")
     args = parser.parse_args()
 
     print('Loading task configuration: ...', end='\r')
@@ -60,7 +81,11 @@ def main():
         inputCfg = yaml.load(ymlCfgFile, yaml.FullLoader)
     print('Loading task configuration: Done!')
 
-    if args.run:
+    if args.run_full:
         run_mft_assesment(inputCfg, "full")
+    if args.merge:
+        merge_files(inputCfg)
+    if args.plot:
+        plots()
 
 main()
