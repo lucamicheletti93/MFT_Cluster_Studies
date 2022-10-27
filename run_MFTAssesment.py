39bda1d9d903ef76ba91affc073cf9c5eaed7d81
Lucamicheletti93 <luca.mike93@gmail.com>
2022-10-27 21:25:01 +0200

    Modifications to run over data

--- run_MFTAssesment.py
+++ run_MFTAssesment.py
@@ -7,16 +7,38 @@ import ROOT
 from ROOT import gROOT, gBenchmark, gPad, gStyle, kTRUE, kFALSE
 from utils.plot_library import LoadStyle, SetLegend
 
+def copy_from_grid(inputCfg):
+    '''
+    function for downloading files from alien grid (Run mft reco. on ctf)
+    '''
+    with open(inputCfg["input"]["run_list_file"]) as f:
+        lines = f.readlines()
+    
+    for i in range(0, len(lines)):
+        if not os.path.isdir("{}{}".format(inputCfg["input"]["ctf_input_path"], i)):
+            os.system("mkdir -p {}{}".format(inputCfg["input"]["ctf_input_path"], i))
+        os.system("alien_cp alien://%s/%s file:%s%i" % (inputCfg["input"]["alien_input_path"], lines[i].replace("\n", ""), inputCfg["input"]["ctf_input_path"], i))
+
+    for i in range(0, len(lines)):
+        os.chdir("%s%i" % (inputCfg["input"]["ctf_input_path"], i))
+        if not os.path.isfile("mftclusters.root") or not os.path.isfile("mfttracks.root"):
+            os.system("o2-ctf-reader-workflow --onlyDet MFT  --ctf-input %s | o2-mft-reco-workflow --clusters-from-upstream --disable-mc  -b" % (lines[i].replace("\n", "")))
+        if not os.path.isfile("o2simdigitizerworkflow_configuration.ini"):
+            os.system("cp /home/lmichele/alice/mft_data/CTF/tf0/o2simdigitizerworkflow_configuration.ini .")
+        if not os.path.isfile("mftdigits.roots"):
+            os.system("cp /home/lmichele/alice/mft_data/CTF/tf0/mftdigits.root .")
+
+
 def run_mft_assesment(inputCfg, mode):
     '''
     function to run the MFTAssesment workflow on time frames
     '''
     paths = []
-    for file in os.listdir(inputCfg["input"]["input_path"]):
-        d = os.path.join(inputCfg["input"]["input_path"], file)
+    for file in os.listdir(inputCfg["input"]["local_input_path"]):
+        d = os.path.join(inputCfg["input"]["local_input_path"], file)
         if os.path.isdir(d):
             paths.append(d)
-            #print(d)
+            print(d)
 
     counter = 0
     for path in paths:
@@ -24,7 +46,10 @@ def run_mft_assesment(inputCfg, mode):
             exit()
         print("----------------------   %s   ----------------------" % (path))
         os.chdir("%s" % (path))
-        os.system("o2-mft-reco-workflow | o2-mft-assessment-workflow")
+        if inputCfg["input"]["prod_type"] == "data":
+            os.system("o2-mft-reco-workflow | o2-mft-assessment-workflow --disable-mc")
+        if inputCfg["input"]["prod_type"] == "mc":
+            os.system("o2-mft-reco-workflow | o2-mft-assessment-workflow")
         counter = counter + 1
         print("----------------------> %s processed" % (path))
 
@@ -34,24 +59,30 @@ def merge_files(inputCfg):
     '''
     paths = []
     counter = 0
-    for file in os.listdir(inputCfg["input"]["input_path"]):
-        d = os.path.join(inputCfg["input"]["input_path"], file)
+    for file in os.listdir(inputCfg["input"]["local_input_path"]):
+        d = os.path.join(inputCfg["input"]["local_input_path"], file)
         if os.path.isdir(d) and os.path.isfile("%s/MFTAssessment.root" % (d)):
             if counter == 0:
-                mergeCommand = "hadd -f MFTAssessment.root "
+                mergeCommand = "hadd -f {} ".format(inputCfg["output"]["local_output_file"])
             mergeCommand += "%s/MFTAssessment.root " % (d)
             counter = counter + 1
     
     print(mergeCommand)
     os.system(mergeCommand)
 
-def analysis():
+def analysis(inputCfg):
     '''
     function for producing QC plots
     '''
     LoadStyle()
 
-    fIn = ROOT.TFile.Open("MFTAssessment.root", "READ")
+    fIn = ROOT.TFile.Open(inputCfg["output"]["local_output_file"], "READ")
+    mMFTTrackMeanClusterSize = fIn.Get("mMFTTrackMeanClusterSize")
+    mMFTTrackMeanClusterSize.SetLineColor(ROOT.kBlack)
+    mMFTTrackMeanClusterSize.Rebin(10)
+    mMFTTrackMeanClusterSize.SetTitle("")
+    mMFTTrackMeanClusterSize.GetXaxis().SetRangeUser(0, 10)
+
     mMFTTrueTrackMeanClusterSize = fIn.Get("mMFTTrueTrackMeanClusterSize")
     mMFTTrueTrackMeanClusterSize.SetLineColor(ROOT.kBlack)
     mMFTTrueTrackMeanClusterSize.Rebin(10)
@@ -91,32 +122,38 @@ def analysis():
     mMFTTrueTrackMeanClusterSizeNonNuclei.Draw("Hsame")
     legend.Draw("same")
     canvas.Update()
-    canvas.SaveAs("mean_cluster_size_per_track.pdf")
+    #canvas.SaveAs("mean_cluster_size_per_track.pdf")
 
-    input()
-
-    #fOut = ROOT.TFile.Open("MyAnalysisResults.root", "RECREATE")
-    #canvas.Write()
-    #fOut.Close()
+    fOut = ROOT.TFile.Open(inputCfg["output"]["analysis_output"], "RECREATE")
+    canvas.Write()
+    mMFTTrackMeanClusterSize.Write()
+    fOut.Close()
 
 def main():
     parser = argparse.ArgumentParser(description='Arguments to pass')
     parser.add_argument('cfgFileName', metavar='text', default='config.yml', help='config file name')
+    parser.add_argument("--download", help="download files from grid", action="store_true")
     parser.add_argument("--run_full", help="run over all the available files", action="store_true")
+    parser.add_argument("--run_test", help="run over a part of the sample", action="store_true")
     parser.add_argument("--merge", help="merge all the files produced", action="store_true")
     parser.add_argument("--analysis", help="plot results", action="store_true")
     args = parser.parse_args()
+
 
     print('Loading task configuration: ...', end='\r')
     with open(args.cfgFileName, 'r') as ymlCfgFile:
         inputCfg = yaml.load(ymlCfgFile, yaml.FullLoader)
     print('Loading task configuration: Done!')
 
+    if args.download:
+        copy_from_grid(inputCfg)
     if args.run_full:
         run_mft_assesment(inputCfg, "full")
+    if args.run_test:
+        run_mft_assesment(inputCfg, "test")
     if args.merge:
         merge_files(inputCfg)
     if args.analysis:
-        analysis()
+        analysis(inputCfg)
 
 main()
