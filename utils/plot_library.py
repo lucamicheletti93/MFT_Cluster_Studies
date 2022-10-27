9ed4eb1531b616edf37a74925ba36156f93ef4aa
Lucamicheletti93 <luca.mike93@gmail.com>
2022-10-27 15:51:30 +0200

    Adding utils repo

---
+++ utils/plot_library.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import array as arr
+import numpy as np
+import os
+import sys
+import argparse
+import ROOT
+from os import path
+from ROOT import TCanvas, TLatex, TF1, TFile, TPaveText, TMath, TH1F, TString, TLegend, TRatioPlot, TGaxis
+from ROOT import gROOT, gBenchmark, gPad, gStyle, kTRUE, kFALSE
+
+def SetLatex(latex):
+    latex.SetTextSize(0.035)
+    latex.SetNDC()
+    latex.SetTextFont(42)
+
+def SetLegend(legend):
+    legend.SetBorderSize(0)
+    legend.SetFillColor(10)
+    legend.SetFillStyle(1)
+    legend.SetLineStyle(0)
+    legend.SetLineColor(0)
+    legend.SetTextFont(42)
+    legend.SetTextSize(0.04)
+
+def LoadStyle():
+    gStyle.SetOptStat(0)
+    gStyle.SetPadLeftMargin(0.15)
+    gStyle.SetPadBottomMargin(0.15)
+    gStyle.SetPadTopMargin(0.05)
+    gStyle.SetPadRightMargin(0.05)
+    gStyle.SetEndErrorSize(0.0)
+    gStyle.SetTitleSize(0.05,"X")
+    gStyle.SetTitleSize(0.045,"Y")
+    gStyle.SetLabelSize(0.045,"X")
+    gStyle.SetLabelSize(0.045,"Y")
+    gStyle.SetTitleOffset(1.2,"X")
+    gStyle.SetTitleOffset(1.35,"Y")
